17b24a94b2fcf7e6810ddc5ddd5d49e071511783
Max_shelll <101334622+max-shelll@users.noreply.github.com>
2022-12-03 16:18:47 +0300

    Add search code Add userAgent

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,13 +1,45 @@
-﻿// Запросим возраст пользователя и сохраним в переменную
-let age = prompt("Пожалуйста, введите ваш возраст");
+﻿// создадим объект Map для хранения сессии
+let session = new Map();
 
-if (age >= 18) {
-    // Те, кто старше 18, увидят приветствие и будут направлены на сайт
-    alert("Приветствуем на LifeSpot! " + new Date().toLocaleString());
+// Сохраним UserAgent
+session.set("userAgent", window.navigator.userAgent)
+
+// Запросим возраст пользователя и тоже сохраним
+session.set("age", prompt("Пожалуйста, введите ваш возраст"))
+
+// Проверка на возраст и сохранение сессии
+if (session.get("age") >= 18) {
+    let startDate = new Date().toLocaleString();
+
+    alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
+    session.set("startDate", startDate)
 }
 else {
-    // Выполним проверку. Если введенное число < 18, либо если введено не число -
-    // пользователь будет перенаправлен
     alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
     window.location.href = "http://www.google.com"
+}
+
+// Вывод в консоль
+for (let result of session) {
+    console.log(result)
+}
+
+
+
+function searchName() {
+    let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
+    let elements = document.getElementsByClassName('video-container');
+
+    for (let i = 0; i <= elements.length; i++)
+    {
+        let videoText = elements[i].querySelector(".video-title").innerText;
+
+        if (!videoText.toLowerCase().includes(inputString.toLowerCase())) {
+            elements[i].style.display = 'none';
+        }
+        else
+        {
+            elements[i].style.display = 'inline-block';
+        }
+    }
 }
