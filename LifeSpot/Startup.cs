81cfbf3926aa471266a749d064e0eaf1e75e88f9
Max_shelll <101334622+max-shelll@users.noreply.github.com>
2022-12-23 15:30:38 +0300

    Add carousel

--- LifeSpot/Startup.cs
+++ LifeSpot/Startup.cs
@@ -22,79 +22,14 @@ namespace LifeSpot
 
             app.UseRouting();
 
-            // Download elements to replace 
-            string footerHtml = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Views", "Shared", "footer.html"));
-            string sideBarHtml = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Views", "Shared", "sideBar.html"));
-
             app.UseEndpoints(endpoints =>
             {
+                // Маппинг статических файлов
 
-                // <!---- HTML ----!> \\
-
-                endpoints.MapGet("/", async context =>
-                {
-                    var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "index.html");
-
-                    var html = new StringBuilder(await File.ReadAllTextAsync(viewPath))
-                        .Replace("<!--SIDEBAR-->", sideBarHtml)
-                        .Replace("<!--FOOTER-->", footerHtml);
-
-                    await context.Response.WriteAsync(html.ToString());
-                });
-
-                endpoints.MapGet("/testing", async context =>
-                {
-                    var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "testing.html");
-
-                    var html = new StringBuilder(await File.ReadAllTextAsync(viewPath))
-                        .Replace("<!--SIDEBAR-->", sideBarHtml)
-                        .Replace("<!--FOOTER-->", footerHtml);
-
-                    await context.Response.WriteAsync(html.ToString());
-                });
-
-                endpoints.MapGet("/about", async context =>
-                {
-                    var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "about.html");
-
-                    var html = new StringBuilder(await File.ReadAllTextAsync(viewPath))
-                        .Replace("<!--SIDEBAR-->", sideBarHtml)
-                        .Replace("<!--FOOTER-->", footerHtml);
-
-                    await context.Response.WriteAsync(html.ToString());
-                });
-
-                // <!---- CSS ----!> \\
-
-                endpoints.MapGet("/Static/CSS/index.css", async context =>
-                {
-                    var cssPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "CSS", "index.css");
-                    var css = await File.ReadAllTextAsync(cssPath);
-                    await context.Response.WriteAsync(css);
-                });
-
-                // <!---- JS ----!> \\
-
-                endpoints.MapGet("/Static/JS/index.js", async context =>
-                {
-                    var jsPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "JS", "index.js");
-                    var js = await File.ReadAllTextAsync(jsPath);
-                    await context.Response.WriteAsync(js);
-                });
-
-                endpoints.MapGet("/Static/JS/testing.js", async context =>
-                {
-                    var jsPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "JS", "testing.js");
-                    var js = await File.ReadAllTextAsync(jsPath);
-                    await context.Response.WriteAsync(js);
-                });
-
-                endpoints.MapGet("/Static/JS/about.js", async context =>
-                {
-                    var jsPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "JS", "about.js");
-                    var js = await File.ReadAllTextAsync(jsPath);
-                    await context.Response.WriteAsync(js);
-                });
+                endpoints.MapImg();
+                endpoints.MapCss();
+                endpoints.MapJs();
+                endpoints.MapHtml();
             });
         }
     }
